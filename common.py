f4330ea9868c363a2ea8900364a3632e85499f0b
Katalin Csortos <katalin.csortos@gmail.com>
2018-10-23 09:34:34 +0200

    common.py includes all the common functions

---
+++ common.py
@@ -0,0 +1,73 @@
+from termcolor import colored
+import csv
+import os
+import random
+import sys
+import termios
+import time
+import tty
+
+import lab_generator
+
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def import_lab_level(filename="labyrinth_orig"):
+    with open("{}.csv".format(filename), "r") as f:
+        new_lab = list(csv.reader(f))
+        for lst in new_lab:
+            for i in range(0, len(lst), 1):
+                lst[i] = int(lst[i])
+        return new_lab
+
+
+def export_random_lab(grid, filename="new_lab.csv"):
+    with open(filename, 'w+') as f:
+        writer = csv.writer(f)
+        for row in grid:
+            writer.writerow(row)
+
+
+def game_intro():
+    os.system('clear')
+    print(r"""
+  _    _                      _                   _    _____          _       _
+ | |  | |                    | |                 | |  / ____|        | |     | |
+ | |__| | __ _ _ __  ___  ___| |   __ _ _ __   __| | | |  __ _ __ ___| |_ ___| |
+ |  __  |/ _` | '_ \/ __|/ _ \ |  / _` | '_ \ / _` | | | |_ | '__/ _ \ __/ _ \ |
+ | |  | | (_| | | | \__ \  __/ | | (_| | | | | (_| | | |__| | | |  __/ ||  __/ |
+ |_|  |_|\__,_|_| |_|___/\___|_|  \__,_|_| |_|\__,_|  \_____|_|  \___|\__\___|_|""")
+    # time.sleep(0.5)
+    # print_slow("\nHansel and Gretel got lost in the evil witch's cellar. Help them to escape.\n")
+    # time.sleep(0.7)
+    # print("You have to eat 4 sweets to escape.")
+    # time.sleep(1.2)
+    # print("You can move with WASD.")
+    time.sleep(1.2)
+    print("When you are ready, press any button to start the game.")
+    start_game = False
+    if getch():
+        start_game = True
+
+
+def cellar_outro():
+    print_slow("\nHansel and Gretel has gotten fat! They walked right into the witch's oven.")
+    time.sleep(1)
+    print_slow(" Congratulations! You killed them!")
+    print("\n")
