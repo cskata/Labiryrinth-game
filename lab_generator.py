f8aa279a800ab9e6d0c38a6cc1731796cb7637e5
Katalin Csortos <katalin.csortos@gmail.com>
2018-10-24 10:36:44 +0200

    renamed variables, added comments

--- lab_generator.py
+++ lab_generator.py
@@ -2,16 +2,16 @@
 from random import shuffle, randrange
 import common
 
-WIDTH = 12
-HEIGHT = 12
-visited_cell = [[0] * WIDTH + [1] for _ in range(HEIGHT)] + [[1] * (WIDTH + 1)]
-# visited_cell is just a utily list that includes 0's and 1's, to track the already visited cells
-horizontal_row = [["111"] * WIDTH + ['1'] for _ in range(HEIGHT + 1)]
-vertical_row = [["100"] * WIDTH + ['1'] for _ in range(HEIGHT)] + [[]]
+WIDTH = 3
+HEIGHT = 3
+visited_cells = [[0] * WIDTH + [1] for _ in range(HEIGHT)] + [[1] * (WIDTH + 1)]
+# visited_cells is just a utily list that includes 0's and 1's, to track the already visited cells
+horizontal_walls = [["111"] * WIDTH + ['1'] for _ in range(HEIGHT + 1)]
+vertical_walls = [["100"] * WIDTH + ['1'] for _ in range(HEIGHT)] + [[]]
 
 
 def walk(x, y):
-    visited_cell[y][x] = 1
+    visited_cells[y][x] = 1
 
     north = (x - 1, y)
     south = (x + 1, y)
@@ -21,12 +21,14 @@ def walk(x, y):
     directions = [north, east, south, west]
     shuffle(directions)
     for (directon_x, direction_y) in directions:
-        if visited_cell[direction_y][directon_x]:
+        if visited_cells[direction_y][directon_x]:
             continue
+        # removes horizontal wall, "111" turns into "100"
         if directon_x == x:
-            horizontal_row[max(y, direction_y)][x] = "100"
+            horizontal_walls[max(y, direction_y)][x] = "100"
+        # removes vertical wall, "100" turns into "000"
         if direction_y == y:
-            vertical_row[y][max(x, directon_x)] = "000"
+            vertical_walls[y][max(x, directon_x)] = "000"
         walk(directon_x, direction_y)
 
 
@@ -35,14 +37,15 @@ def append_rows_to_maze(hor, ver):
     maze_row = zip(hor, ver)
     for (a, b) in maze_row:
         full_maze.append((''.join(a)))
+        # vertical row must be appended twice for wide corridors
         full_maze.append((''.join(b)))
-        full_maze.append((''.join(b)))    # vertical row must be appended twice for wide corridors
+        full_maze.append((''.join(b)))
     return full_maze
 
 
 def create_random_maze():
     walk(randrange(WIDTH), randrange(HEIGHT))
-    full_maze = append_rows_to_maze(horizontal_row, vertical_row)
+    full_maze = append_rows_to_maze(horizontal_walls, vertical_walls)
     full_maze.pop()
     return full_maze
 
@@ -50,3 +53,6 @@ def create_random_maze():
 def make_new_lab():
     labyrinth = create_random_maze()
     common.export_random_lab(labyrinth)
+
+
+make_new_lab()
