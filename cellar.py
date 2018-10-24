35a50591b85dff47e2363743e122abefd5f1a9ed
Katalin Csortos <katalin.csortos@gmail.com>
2018-10-24 14:28:21 +0200

    Added functions to 'open and close' the cellar gates

--- cellar.py
+++ cellar.py
@@ -26,31 +26,29 @@ def collectable_sweets():
 CELLAR_ITEMS = {
     'CORRIDOR': [0, '  '],
     'WALL': [1, '\u2588\u2588'],
-    'SPAWNED_ITEM': [2, collectable_sweets(), 1],
+    'SPAWNED_ITEM': [2, collectable_sweets(), 2],
     'GATE': [3, '\u2588\u2588', 'red'],
     'PLAYER': [4, '\u2659\u265f']
     }
 
 
-def place_and_close_gates():
-    pass
-
-
-def open_gates():
-    pass
-
-
-def add_sweets_and_gates(lab_width, lab_height, generated_keys=0):
-    sweets = CELLAR_ITEMS['SPAWNED_ITEM'][2]
-    lab_generator.make_new_lab()
-    labyrinth = common.import_lab_level("new_lab")
-
+def place_and_close_gates(labyrinth):
     labyrinth[0][2] = CELLAR_ITEMS['GATE'][0]
     labyrinth[0][3] = CELLAR_ITEMS['GATE'][0]
-    labyrinth[lab_height][lab_width - 2] = CELLAR_ITEMS['GATE'][0]
-    labyrinth[lab_height][lab_width - 1] = CELLAR_ITEMS['GATE'][0]
+    labyrinth[LAB_HEIGHT][LAB_WIDTH - 2] = CELLAR_ITEMS['GATE'][0]
+    labyrinth[LAB_HEIGHT][LAB_WIDTH - 1] = CELLAR_ITEMS['GATE'][0]
 
-    while generated_keys != sweets:
+
+def open_gates(labyrinth):
+    labyrinth[LAB_HEIGHT][LAB_WIDTH - 2] = CELLAR_ITEMS['CORRIDOR'][0]
+    labyrinth[LAB_HEIGHT][LAB_WIDTH - 1] = CELLAR_ITEMS['CORRIDOR'][0]
+
+
+def create_cellar_with_sweets(lab_width, lab_height, generated_keys=0):
+    lab_generator.make_new_lab()
+    labyrinth = common.import_lab_level("new_lab")
+    spawned_sweets = CELLAR_ITEMS['SPAWNED_ITEM'][2]
+    while generated_keys != spawned_sweets:
         key_a = random.randint(0, lab_height)
         key_b = random.randint(0, lab_width)
         if labyrinth[key_a][key_b] == 0:
@@ -64,12 +62,7 @@ def add_sweets_and_gates(lab_width, lab_height, generated_keys=0):
 
 def draw(labyrinth, collected_sweets, sweets_to_collect):
     os.system('clear')
-    if collected_sweets != sweets_to_collect:
-        print(f"Eat all the {sweets_to_collect} sweets and you can escape through the gate below!\n")
-    else:
-        print("You ate all the sweets. The gate is now open, you can escape!\n")
-        open_gates()
-    print_how_many_sweets_left(collected_sweets, sweets_to_collect)
+    print_how_many_sweets_left(labyrinth, collected_sweets, sweets_to_collect)
     i = 0
     while i < len(labyrinth):
         j = 0
@@ -147,7 +140,12 @@ def are_they_escaped_from_cellar(labyrinth):
     return game_over
 
 
-def print_how_many_sweets_left(colld_sw, sw_to_coll):
+def print_how_many_sweets_left(labyrinth, colld_sw, sw_to_coll):
+    if colld_sw != sw_to_coll:
+        print(f"Eat all the {sw_to_coll} sweets and you can escape through the gate below!\n")
+    else:
+        print("You ate all the sweets. The gate is now open, you can escape!\n")
+        open_gates(labyrinth)
     print("You already ate {} sweet(s). Good.".format(colld_sw))
     print("You must eat {} more. Hurry-hurry!\n".format(sw_to_coll - colld_sw))
 
@@ -155,21 +153,18 @@ def print_how_many_sweets_left(colld_sw, sw_to_coll):
 def main():
     common.game_intro()
     common.cellar_intro()
-    add_sweets_and_gates(LAB_WIDTH, LAB_HEIGHT)
+    create_cellar_with_sweets(LAB_WIDTH, LAB_HEIGHT)
     labyrinth = common.import_lab_level("cellar")
+    place_and_close_gates(labyrinth)
     labyrinth[1][3] = CELLAR_ITEMS['PLAYER'][0]
     sweets_to_collect = CELLAR_ITEMS['SPAWNED_ITEM'][2]
     while not are_they_escaped_from_cellar(labyrinth):
         collected_sweets = count_uncollected_sweets(labyrinth)
         draw(labyrinth, collected_sweets, sweets_to_collect)
-        if collected_sweets == sweets_to_collect:
-            labyrinth[LAB_HEIGHT][LAB_WIDTH - 2] = CELLAR_ITEMS['CORRIDOR'][0]
-            labyrinth[LAB_HEIGHT][LAB_WIDTH - 1] = CELLAR_ITEMS['CORRIDOR'][0]
-            draw(labyrinth, collected_sweets, sweets_to_collect)
         labyrinth = move_player(labyrinth)
     else:
         draw(labyrinth, collected_sweets, sweets_to_collect)
-        # common.cellar_outro()
+        common.cellar_outro()
         # új szinthez ide kéne meghívni esetleg a következő szint mainjét?
 
 
