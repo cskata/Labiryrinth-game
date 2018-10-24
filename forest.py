e7acad6c1f19002a7fec44267fe6762f8e447f8e
Katalin Csortos <katalin.csortos@gmail.com>
2018-10-25 00:40:12 +0200

    Implemented random tree generator in printed forest

---
+++ forest.py
@@ -0,0 +1,79 @@
+from termcolor import colored
+import os
+import random
+import sys
+import termios
+import time
+import tty
+
+import lab_generator
+import common
+
+FOREST_WIDTH = 20
+FOREST_HEIGTH = 20
+
+
+FOREST_ITEMS = {
+    'CORRIDOR': [0, ' '],
+    'EDGE': [1, '\U0001F332'],
+    'SPAWNED_ITEM': [2, '@', 4]
+}
+
+
+def create_forest_area():
+    # 1 = \U0001F332
+    # 0 = "   "
+    forest = []
+    edge = ("1" * (FOREST_WIDTH) + "11")
+    gameplay_area = ("1" + ("00" * (FOREST_WIDTH)) + "1")
+    forest.append(edge)
+    for _ in range(FOREST_HEIGTH):
+        forest.append(gameplay_area)
+    forest.append(edge)
+    common.export_random_lab(forest, "forest")
+    forest = common.import_lab_level("forest")
+    return forest
+
+
+def print_forest(forest):
+    os.system('clear')
+    forest = common.import_lab_level("forest")
+    for x, row in enumerate(forest):
+        for y, cell in enumerate(row):
+            for k in FOREST_ITEMS.keys():
+                if FOREST_ITEMS[k][0] == forest[x][y]:
+                    key = k
+            if forest[x][y] == FOREST_ITEMS[key][0]:
+                sys.stdout.write(FOREST_ITEMS[key][1])
+        print()
+
+
+def spawn_trees(spawned_trees=0):
+    # tree code: \U0001F333
+    # find the tree where a sword is hidden
+    trees_to_spawn = FOREST_ITEMS['SPAWNED_ITEM'][2]
+    forest = create_forest_area()
+    while spawned_trees != trees_to_spawn:
+        x = random.randint(0, (FOREST_WIDTH))
+        y = random.randint(0, (FOREST_HEIGTH))
+        if forest[x][y] == FOREST_ITEMS['CORRIDOR'][0]:
+            forest[x][y] = FOREST_ITEMS['SPAWNED_ITEM'][0]
+            spawned_trees += 1
+        else:
+            spawned_trees += 0
+    common.export_random_lab(forest, "forest")
+    forest = common.import_lab_level("forest")
+    return forest
+
+
+def spawn_enemies():
+    pass
+
+
+def main():
+    forest = spawn_trees()
+    print_forest(forest)
+
+
+if __name__ == '__main__':
+    main()
